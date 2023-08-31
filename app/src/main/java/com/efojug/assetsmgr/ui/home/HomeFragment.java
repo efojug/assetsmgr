e60278eec6bc1d9bc7a669531d9d71e4734d1fa9
efojug <lidudu890890@163.com>
2023-09-01 04:09:27 +0800

    下拉框

--- app/src/main/java/com/efojug/assetsmgr/ui/home/HomeFragment.java
+++ app/src/main/java/com/efojug/assetsmgr/ui/home/HomeFragment.java
@@ -5,7 +5,10 @@ import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,13 +24,18 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private FragmentHomeBinding binding;
     private EditText expenseNameEditText;
     private EditText expenseAmountEditText;
+    private Spinner expenseTypeSpinner;
+    private List<String> mExpenseType;
+    private ArrayAdapter<String> mAdapter;
 
     private void showToast(String text) {
         Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
@@ -69,6 +77,28 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         View root = binding.getRoot();
         expenseNameEditText = root.findViewById(R.id.expense_name_edittext);
         expenseAmountEditText = root.findViewById(R.id.expense_amount_edittext);
+        expenseTypeSpinner = root.findViewById(R.id.expense_type_spinner);
+        mExpenseType = new ArrayList<>();
+        mExpenseType.add("1");
+        mExpenseType.add("2");
+        mExpenseType.add("3");
+
+        mAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, mExpenseType);
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        expenseTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String item = mAdapter.getItem(position);
+                showToast(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //do nothing
+            }
+        });
+
         root.findViewById(R.id.add_expense_button).setOnClickListener(this);
 //        getExpenseDataFile();
         return root;
