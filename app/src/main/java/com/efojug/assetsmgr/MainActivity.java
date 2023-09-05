3815c10a3ff45f392ec3e217df4b981fcbd3e645
efojug <lidudu890890@163.com>
2023-09-05 19:13:00 +0800

    fuck testOnly

--- app/src/main/java/com/efojug/assetsmgr/MainActivity.java
+++ app/src/main/java/com/efojug/assetsmgr/MainActivity.java
@@ -14,9 +14,8 @@ import com.efojug.assetsmgr.databinding.ActivityMainBinding;
 /* TODO List
     Dashboard 右上角调整月份，以月份显示图表 -> efojug
     Dashboard ExpenseList 左滑修改和删除 -> DeSu, efojug
-    Dashboard ProgressBar 和 顶部Text 添加上月结转 -> efojug
+    Dashboard ProgressBar 和 顶部 TextView 添加上月结转 -> efojug
     Settings 结转 -> efojug, DeSu
-    Main 月初提醒获取生活费 -> efojug
     Main 数据导出
  */
 
