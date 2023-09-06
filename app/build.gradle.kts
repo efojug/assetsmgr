71d5aeebc643db87e89bbb80ae17f59d31f3fa01
efojug <lidudu890890@163.com>
2023-09-06 18:46:20 +0800

    upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,15 +67,14 @@ dependencies {
     implementation(libs.lottie.compose)
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.10.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.compose.material:material-android:1.5.0")
 
