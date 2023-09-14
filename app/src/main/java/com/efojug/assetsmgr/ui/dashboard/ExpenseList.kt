6cdfcc54a0a486dbd016fe05288e2ee444c53d4b
efojug <lidudu890890@163.com>
2023-09-15 00:53:23 +0800

    we need more code

--- app/src/main/java/com/efojug/assetsmgr/ui/dashboard/ExpenseList.kt
+++ app/src/main/java/com/efojug/assetsmgr/ui/dashboard/ExpenseList.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
